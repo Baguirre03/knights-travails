49d80ccd70171c85275c35babcd238968dad60f8
Ben <baguirre@luc.edu>
2023-07-22 23:02:51 -0500

    q update

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ class Node {
         this.two = null
         this.three = null
         this.four = null
-        this.five = null
-        this.six = null
-        this.seven = null
-        this.eight = null
+        // this.five = null
+        // this.six = null
+        // this.seven = null
+        // this.eight = null
     }
 }
 
@@ -44,21 +44,18 @@ function knightMoves(pieceStart, pieceEnd) {
 
     let tree = buildTree(game.board[root], game.board, pieceEnd)
     console.log(tree)
-
 }
-
-// options = 6, 10, 15, 17 
 
 
 function buildTree(array, board, end) {
     let root = new Node(array)
+    let index = findIndexOfStart(array, board)
 
+    if (index == null) return null
     if (array[0] * array[1] > 64) return null
     if (array[0 * array[1] < 1]) return null
-    if (findIndexOfStart(array, board) == null) {
-        return null
-    }
     if (array[0] == end[0] && array[1] == end[1]) { return root }
+
 
     let options = [
         [array[0] + 1, array[1] + 2], //one
@@ -71,16 +68,15 @@ function buildTree(array, board, end) {
         [array[0] - 1, array[1] + 2] //eight
     ]
 
-    root.one = new Node(buildTree(options[0], board, end))
-    root.two = new Node(buildTree(options[1], board, end))
-    root.three = new Node(buildTree(options[2], board, end))
-    root.four = new Node(buildTree(options[3], board, end))
-    // root.five = new Node(buildTree(options[4], board, end))
-    // root.six = new Node(buildTree(options[5], board, end))
-    // root.seven = new Node(buildTree(options[6], board, end))
-    // root.eight = new Node(buildTree(options[7], board, end))
-
+    root.one = buildTree(options[0], board, end)
+    root.two = buildTree(options[1], board, end)
+    root.three = buildTree(options[2], board, end)
+    root.four = buildTree(options[3], board, end)
+    root.five = new Node(buildTree(options[4], board, end))
+    root.six = new Node(buildTree(options[5], board, end))
+    root.seven = new Node(buildTree(options[6], board, end))
+    root.eight = new Node(buildTree(options[7], board, end))
     return root
 }
 
-knightMoves([1, 1], [2, 3])
+knightMoves([1, 1], [4, 4])
