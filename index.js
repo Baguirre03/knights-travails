b14ed98d7a09d0ec24b69e1315f9cb83c70b9209
Ben <baguirre@luc.edu>
2023-08-01 12:11:42 -0500

    error testing exampels

--- index.js
+++ index.js
@@ -32,20 +32,29 @@ function findIndexOfStart(find, board) {
 
 
 function knightMoves(pieceStart, pieceEnd) {
-    let game = new Game(pieceStart)
+    if (pieceStart[0] > 8 || pieceStart[0] < 1 || pieceStart[1] > 8 || pieceStart[1] < 1
+        || pieceEnd[0] > 8 || pieceEnd[0] < 1 || pieceEnd[1] > 8 || pieceEnd[1] < 1) {
+        return 'Piece isnt on board!'
+    } else if (typeof pieceStart != 'object' || typeof pieceEnd != 'object') {
+        return 'Please enter an array!'
+    }
 
+    let game = new Game(pieceStart)
     let root = findIndexOfStart(game.knight, game.board)
     let start = { name: 'root', array: game.board[root] }
-
     let final = buildTree(start, game.board, pieceEnd)
+
     if (!final.prev) {
-        return final
+        return formatFinal('', final.data)
     }
     return formatFinal(final.prev, final.data)
 }
 
 function formatFinal(array, last) {
     let message = 'Path Taken: '
+    if (!array) {
+        return message += `-> [${last}] -> [${last}] (doesnt move)`
+    }
     array.forEach((array) => {
         message += `-> [${array}] `
     })
@@ -103,7 +112,6 @@ function buildTree(array, board, end, queue = ['fill']) {
         let b = root.data
         for (let i = 0; i < a.length; i++) {
             if (a[i][0] === b[0] && a[i][1] === b[1]) {
-                // queue.splice(1)
                 return buildTree(queue[0], board, end, queue)
             }
         }
@@ -129,13 +137,20 @@ function buildTree(array, board, end, queue = ['fill']) {
         array.parent[array.name] = root
     }
 
-
     queue.push(options.one, options.two, options.three, options.four, options.five, options.six, options.seven, options.eight)
     return buildTree(queue[0], board, end, queue)
 }
 
-// console.log(knightMoves([1, 1], [2, 3]))
-// console.log(knightMoves([1, 1], [4, 4]))
-// console.log(knightMoves([3, 3], [4, 3]))
-console.log(knightMoves([1, 8], [8, 1]))
+console.log(knightMoves([1, 1], [2, 3]))
+console.log(knightMoves([1, 1], [4, 4]))
+console.log(knightMoves([3, 3], [4, 3]))
+console.log(knightMoves([1, 8], [1, 8]))
+//error testing
+console.log(knightMoves([4, 13], [1, 8]))
+console.log(knightMoves('', [1, 8]))
+console.log(knightMoves([1, 1], [1, 0]))
+console.log(knightMoves('hello', [1, 1]))
+
+
+
 
